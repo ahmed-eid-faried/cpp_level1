528d5d1595a01f0814f3bd12a6262c7b76024cc0
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-18 17:06:01 +0300

    50WhileLoop

---
+++ MYFIRSTAPP/50WhileLoop_2.cpp
@@ -0,0 +1,47 @@
+//#include <iostream>
+//#include <sstream>
+//#include <string>
+//#include <typeinfo>
+//using namespace std;
+//
+//template <typename T, typename... Args>
+//void input(T& variable, Args... prompts) {
+//	string betweenSpace = "";
+//	string inputStr;
+//	// œ„Ã Ã„Ì⁄ «·‰’Ê’ ›Ì ”·”·… Ê«Õœ…
+//	string fullPrompt;
+//	(void)initializer_list<int>{ (fullPrompt.append(prompts), fullPrompt.append(betweenSpace), 0)... };
+//	cout << fullPrompt;
+//	getline(cin, inputStr);
+//	stringstream ss(inputStr);
+//	T temp;
+//	if (!(ss >> temp)) {
+//		cerr << "Invalid input type for " << typeid(T).name() << "\n";
+//	}
+//	else {
+//		variable = temp;
+//	}
+//}
+//
+//void InputPositiveNumber() {
+//	int NUMBER;
+//	input(NUMBER, "PLEASE ENTER POSITIVE NUMBER: ");
+//	while (NUMBER < 0) {
+//		input(NUMBER, "THIS IS NOT POSITIVE NUMBER\nPLEASE REENTER POSITIVE NUMBER: ");
+//	}
+//	cout << "#################################################\n";
+//	cout << "YOUR NUMBER IS ########>>>>  " << NUMBER << endl;
+//}
+//
+//
+//int main() {
+//	cout << "#################################################\n";
+//	cout << "################## AHMED  MADY ##################\n";
+//	cout << "#################################################\n";
+//	InputPositiveNumber();
+//	cout << "#################################################\n";
+//	cout << "#################################################\n";
+//	return 0;
+//}
+//
+//
---
+++ MYFIRSTAPP/50WhileLoop_3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <typeinfo>
+using namespace std;
+
+template <typename T, typename... Args>
+void input(T& variable, Args... prompts) {
+	string betweenSpace = "";
+	string inputStr;
+	// œ„Ã Ã„Ì⁄ «·‰’Ê’ ›Ì ”·”·… Ê«Õœ…
+	string fullPrompt;
+	(void)initializer_list<int>{ (fullPrompt.append(prompts), fullPrompt.append(betweenSpace), 0)... };
+	cout << fullPrompt;
+	getline(cin, inputStr);
+	stringstream ss(inputStr);
+	T temp;
+	if (!(ss >> temp)) {
+		cerr << "Invalid input type for " << typeid(T).name() << "\n";
+	}
+	else {
+		variable = temp;
+	}
+}
+void InputNumberInRange(int From, int To) {
+	int NUMBER;
+	input(NUMBER, "PLEASE ENTER NUMBER BETWEEN ", to_string(From), " TO ", to_string(To), ": ");
+	while (NUMBER < From || NUMBER>To) {
+		input(NUMBER, "THIS IS NOT POSITIVE NUMBER", "\nPLEASE REENTER POSITIVE NUMBER: ");
+	}
+	cout << "#################################################\n";
+	cout << "YOUR NUMBER IS ########>>>>  " << NUMBER << endl;
+}
+
+
+int main() {
+	cout << "#################################################\n";
+	cout << "################## AHMED  MADY ##################\n";
+	cout << "#################################################\n";
+	InputNumberInRange(23, 77);
+
+	cout << "#################################################\n";
+	cout << "#################################################\n";
+	return 0;
+}
+
+
