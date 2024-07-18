e2702f289aac6dea308dd7d9b26fd77d01e6fff4
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-18 17:17:26 +0300

    InputPositiveNumber by do while

---
+++ MYFIRSTAPP/51DoWhileLoop_1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <typeinfo>
+using namespace std;
+
+template <typename T, typename... Args>
+void input(T& variable, Args... prompts) {
+	string betweenSpace = "";
+	string inputStr;
+	// œ„Ã Ã„Ì⁄ «·‰’Ê’ ›Ì ”·”·… Ê«Õœ…
+	string fullPrompt;
+	(void)initializer_list<int>{ (fullPrompt.append(prompts), fullPrompt.append(betweenSpace), 0)... };
+	cout << fullPrompt;
+	getline(cin, inputStr);
+	stringstream ss(inputStr);
+	T temp;
+	if (!(ss >> temp)) {
+		cerr << "Invalid input type for " << typeid(T).name() << "\n";
+	}
+	else {
+		variable = temp;
+	}
+}
+
+void InputPositiveNumber() {
+	int NUMBER;
+	do {
+		input(NUMBER, "PLEASE ENTER POSITIVE NUMBER: ");
+	} while (NUMBER < 0);
+	cout << "#################################################\n";
+	cout << "YOUR NUMBER IS ########>>>>  " << NUMBER << endl;
+}
+
+
+int main() {
+	cout << "#################################################\n";
+	cout << "################## AHMED  MADY ##################\n";
+	cout << "#################################################\n";
+	InputPositiveNumber();
+	cout << "#################################################\n";
+	cout << "#################################################\n";
+	return 0;
+}
+
+
