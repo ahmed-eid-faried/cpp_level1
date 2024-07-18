d7877b343c722140db2a16624c721ecf55dc7957
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-18 17:20:11 +0300

    fix InputNumberInRange by while

--- MYFIRSTAPP/51DoWhileLoop_2.cpp
+++ MYFIRSTAPP/51DoWhileLoop_2.cpp
@@ -1,48 +1,48 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <typeinfo>
-using namespace std;
-
-template <typename T, typename... Args>
-void input(T& variable, Args... prompts) {
-	string betweenSpace = "";
-	string inputStr;
-	// œ„Ã Ã„Ì⁄ «·‰’Ê’ ›Ì ”·”·… Ê«Õœ…
-	string fullPrompt;
-	(void)initializer_list<int>{ (fullPrompt.append(prompts), fullPrompt.append(betweenSpace), 0)... };
-	cout << fullPrompt;
-	getline(cin, inputStr);
-	stringstream ss(inputStr);
-	T temp;
-	if (!(ss >> temp)) {
-		cerr << "Invalid input type for " << typeid(T).name() << "\n";
-	}
-	else {
-		variable = temp;
-	}
-}
-
-
-void InputNumberInRange(int From, int To) {
-	int NUMBER;
-	do {
-		input(NUMBER, "PLEASE ENTER NUMBER BETWEEN ", to_string(From), " TO ", to_string(To), ": ");
-	} while (NUMBER < From || NUMBER>To);
-	cout << "#################################################\n";
-	cout << "YOUR NUMBER IS ########>>>>  " << NUMBER << endl;
-}
-
-
-int main() {
-	cout << "#################################################\n";
-	cout << "################## AHMED  MADY ##################\n";
-	cout << "#################################################\n";
-	InputNumberInRange(0, 100);
-
-	cout << "#################################################\n";
-	cout << "#################################################\n";
-	return 0;
-}
-
-
+//#include <iostream>
+//#include <sstream>
+//#include <string>
+//#include <typeinfo>
+//using namespace std;
+//
+//template <typename T, typename... Args>
+//void input(T& variable, Args... prompts) {
+//	string betweenSpace = "";
+//	string inputStr;
+//	// œ„Ã Ã„Ì⁄ «·‰’Ê’ ›Ì ”·”·… Ê«Õœ…
+//	string fullPrompt;
+//	(void)initializer_list<int>{ (fullPrompt.append(prompts), fullPrompt.append(betweenSpace), 0)... };
+//	cout << fullPrompt;
+//	getline(cin, inputStr);
+//	stringstream ss(inputStr);
+//	T temp;
+//	if (!(ss >> temp)) {
+//		cerr << "Invalid input type for " << typeid(T).name() << "\n";
+//	}
+//	else {
+//		variable = temp;
+//	}
+//}
+//
+//
+//void InputNumberInRange(int From, int To) {
+//	int NUMBER;
+//	do {
+//		input(NUMBER, "PLEASE ENTER NUMBER BETWEEN ", to_string(From), " TO ", to_string(To), ": ");
+//	} while (NUMBER < From || NUMBER>To);
+//	cout << "#################################################\n";
+//	cout << "YOUR NUMBER IS ########>>>>  " << NUMBER << endl;
+//}
+//
+//
+//int main() {
+//	cout << "#################################################\n";
+//	cout << "################## AHMED  MADY ##################\n";
+//	cout << "#################################################\n";
+//	InputNumberInRange(0, 100);
+//
+//	cout << "#################################################\n";
+//	cout << "#################################################\n";
+//	return 0;
+//}
+//
+//
